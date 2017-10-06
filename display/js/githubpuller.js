b972d92f6946bed6e60187bdc8100b49764557cf
ZiqueYuutaka <izferren@gmail.com>
2017-10-05 21:26:53 -0400

    saving

---
+++ display/js/githubpuller.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    !function(config) {
+        $('.zique-tag').each(function() {
+            var tagName = $(this).data('zique-tag');
+            $(this).load(config.prefix + tagName + '.html?cb=' + (new Date().getTime()));
+        });
+    }({
+        prefix: 'https://github.com/ZiqueYuutaka/WebContent'
+    });
+});
